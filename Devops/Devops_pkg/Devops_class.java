b8d00b91807788d768a1d81e17a199388356fbc6
murugesan <murugesanpmc@hotmail.com>
2018-03-22 20:08:58 +0530

    Second commit

--- Devops/Devops_pkg/Devops_class.java
+++ Devops/Devops_pkg/Devops_class.java
@@ -4,6 +4,7 @@ public class Devops_class {
 	public static void main(String[] args) {
 	      // Prints "Hello, World" in the terminal window.
 	      System.out.println("Hello, World");
+	      System.out.println("Hello, Muru");
 	   }
 
 }
