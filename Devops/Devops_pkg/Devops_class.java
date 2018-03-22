f6b95bad9126a56039d08fd9c700a52d2a26cbec
murugesan <murugesanpmc@hotmail.com>
2018-03-22 20:06:00 +0530

    First commit

---
+++ Devops/Devops_pkg/Devops_class.java
@@ -0,0 +1,9 @@
+package Devops_pkg;
+
+public class Devops_class {
+	public static void main(String[] args) {
+	      // Prints "Hello, World" in the terminal window.
+	      System.out.println("Hello, World");
+	   }
+
+}
